52ef88ded843a3114f1ba37a2994908e1a88305a
JeanPierre-76 <jpgiusto@yahoo.com>
2022-10-06 23:57:31 -0300

    Add node version

---
+++ app/helpers/painters_helper.rb
@@ -0,0 +1,6 @@
+module PaintersHelper
+
+
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  resources :products
+  resources :brands
+  resources :painters do
+    resources :pictures
+  end
+
+  #get 'pictures/listar'
+  #get 'pictures/nuevo_radio'
+  #get 'pictures/nuevo_select'
+  #post 'pictures', to: 'pictures#create'
+
+  resources :notices
+  get 'pages/home'
+  devise_for :users
+  resources :dogs
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+
+  root "products#index"
+end
